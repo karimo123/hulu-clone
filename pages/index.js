e3a49a8144f7a1834255167c843acb235034a513
karimo123 <kgher047@uottawa.ca>
2022-04-22 18:58:32 -0400

    changed header name to Header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../components/header'
+import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
 import requests from '../utils/requests'
